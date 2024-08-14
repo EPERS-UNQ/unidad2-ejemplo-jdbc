9b00c0360d87799feac0b81990bcdd0d7b515e07
Fabian Frangella <fabianfrangella@gmail.com>
2024-08-13 21:01:11 -0300

    typo

--- src/main/java/ar/edu/unq/unidad1/wop/modelo/exception/MuchoPesoException.java
+++ src/main/java/ar/edu/unq/unidad1/wop/modelo/exception/MuchoPesoException.java
@@ -5,6 +5,6 @@ import ar.edu.unq.unidad1.wop.modelo.Personaje;
 
 public class MuchoPesoException extends RuntimeException {
     public MuchoPesoException(Personaje personaje, Item item) {
-        super("El personaje [" + personaje + "] no puede recoger [" + item + "] porque cagar mucho peso ya");
+        super("El personaje [" + personaje + "] no puede recoger [" + item + "] porque carga mucho peso ya");
     }
 }
