19f1d9d6e82aa4cc915598eae9ddc0fd3bfafdd5
Ivan <ivan@getcrowder.com>
2025-08-20 23:35:06 -0300

    Se dividio en subtareas algunos metodos medio choclos del jdbcDAO

--- src/main/java/ar/edu/unq/unidad1/wop/dao/impl/JDBCPersonajeDAO.java
+++ src/main/java/ar/edu/unq/unidad1/wop/dao/impl/JDBCPersonajeDAO.java
@@ -4,9 +4,10 @@ import ar.edu.unq.unidad1.wop.dao.PersonajeDAO;
 import ar.edu.unq.unidad1.wop.modelo.Personaje;
 
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashSet;
 
@@ -19,56 +20,75 @@ public record JDBCPersonajeDAO() implements PersonajeDAO {
     public void guardar(Personaje personaje) {
         JDBCConnector.getInstance().execute(conn  -> {
             try {
-                var ps = conn.prepareStatement("INSERT INTO personaje (nombre, pesoMaximo, xp, vida) VALUES (?,?,?,?)");
-                ps.setString(1, personaje.getNombre());
-                ps.setInt(2, personaje.getPesoMaximo());
-                ps.setInt(3, personaje.getXp());
-                ps.setInt(4, personaje.getVida());
-                //ojo, no estamos guardando el inventario!
+                var ps = prepareInsertQueryStatement(personaje, conn);
                 return ps.execute();
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
         });
+    }
+
+    private static PreparedStatement prepareInsertQueryStatement(Personaje personaje, Connection conn) throws SQLException {
+        var ps = conn.prepareStatement("INSERT INTO personaje (nombre, pesoMaximo, xp, vida) VALUES (?,?,?,?)");
+        ps.setString(1, personaje.getNombre());
+        ps.setInt(2, personaje.getPesoMaximo());
+        ps.setInt(3, personaje.getXp());
+        ps.setInt(4, personaje.getVida());
+        //ojo, no estamos guardando el inventario!
+        return ps;
     }
 
     public Personaje recuperar(String nombre) {
         return JDBCConnector.getInstance().execute( conn -> {
             try {
-                var ps = conn.prepareStatement("SELECT pesoMaximo, xp, vida FROM personaje WHERE nombre = ?");
-                ps.setString(1, nombre);
+                var ps = prepareSelectQueryStatement(nombre, conn);
                 var resultSet = ps.executeQuery();
-                Personaje personaje = null;
-                while (resultSet.next()) {
-                    //si personaje no es null aca significa que el while dio mas de una vuelta, eso
-                    //suele pasar cuando el resultado (resultset) tiene mas de un elemento.
-                    if (personaje != null) {
-                        throw new RuntimeException(String.format("Existe mas de un personaje con el nombre %s", nombre));
-                    }
-                    personaje = new Personaje(nombre,
-                            resultSet.getInt("pesoMaximo"),
-                            resultSet.getInt("xp"),
-                            resultSet.getInt("vida"),
-                            new HashSet<>()
-                    );
-                }
-                return personaje;
+                return buildPersonaje(nombre, resultSet);
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
         });
     }
 
+    private static PreparedStatement prepareSelectQueryStatement(String nombre, Connection conn) throws SQLException {
+        var ps = conn.prepareStatement("SELECT pesoMaximo, xp, vida FROM personaje WHERE nombre = ?");
+        ps.setString(1, nombre);
+        return ps;
+    }
+
+    private static Personaje buildPersonaje(String nombre, ResultSet resultSet) throws SQLException {
+        Personaje personaje = null;
+        while (resultSet.next()) {
+            //si personaje no es null aca significa que el while dio mas de una vuelta, eso
+            //suele pasar cuando el resultado (resultset) tiene mas de un elemento.
+            if (personaje != null) {
+                throw new RuntimeException(String.format("Existe mas de un personaje con el nombre %s", nombre));
+            }
+            personaje = new Personaje(nombre,
+                    resultSet.getInt("pesoMaximo"),
+                    resultSet.getInt("xp"),
+                    resultSet.getInt("vida"),
+                    new HashSet<>()
+            );
+        }
+        return personaje;
+    }
+
     public void eliminar(Personaje personaje) {
         JDBCConnector.getInstance().execute (conn -> {
             try {
-                var ps = conn.prepareStatement("DELETE FROM personaje WHERE nombre =  ? ");
-                ps.setString(1, personaje.getNombre());
+                var ps = prepareDeleteQueryStatement(personaje, conn);
                 return ps.execute();
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
         });
+    }
+
+    private static PreparedStatement prepareDeleteQueryStatement(Personaje personaje, Connection conn) throws SQLException {
+        var ps = conn.prepareStatement("DELETE FROM personaje WHERE nombre =  ? ");
+        ps.setString(1, personaje.getNombre());
+        return ps;
     }
 
     public JDBCPersonajeDAO() {
